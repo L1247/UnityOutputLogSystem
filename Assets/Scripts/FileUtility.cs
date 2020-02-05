82363e8f20f0de0ce6d4e3b454599b142e5ce77c
rStar <starlove124760@gmail.com>
2020-02-06 04:47:22 +0800

    Added - NameSpace

--- Assets/Scripts/FileUtility.cs
+++ Assets/Scripts/FileUtility.cs
@@ -2,37 +2,40 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public static class FileUtility
+namespace UnityOutputLogSystem
 {
-    public static string ReadFile(string foldPath ,  string fileName)
+    public static class FileUtility
     {
-        CheckFoldPath(foldPath);
-        StreamReader streamReader = new StreamReader(Path.Combine(foldPath, fileName), true);
-        var content = streamReader.ReadToEnd();
-        streamReader.Close();
-        return content;
-    }
+        public static string ReadFile(string foldPath , string fileName)
+        {
+            CheckFoldPath(foldPath);
+            StreamReader streamReader = new StreamReader(Path.Combine(foldPath, fileName), true);
+            var          content      = streamReader.ReadToEnd();
+            streamReader.Close();
+            return content;
+        }
     
-    public static void WriteFile(string foldPath, string text, string fileName)
-    {
-        CheckFoldPath(foldPath);
-        StreamWriter streamWriter = new StreamWriter(Path.Combine(foldPath, fileName), true);
-        streamWriter.WriteLine(text);
-        streamWriter.Close();
-    }
+        public static void WriteFile(string foldPath, string text, string fileName)
+        {
+            CheckFoldPath(foldPath);
+            StreamWriter streamWriter = new StreamWriter(Path.Combine(foldPath, fileName), true);
+            streamWriter.WriteLine(text);
+            streamWriter.Close();
+        }
 
-    public static void CaptureScreenShot(string foldPath , int exceptionIndex)
-    {
-        CheckFoldPath(foldPath);
-        var fileInfo = $"{exceptionIndex}-" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
-        var filename = Path.Combine(foldPath, fileInfo);
-        ScreenCapture.CaptureScreenshot(filename);
-    }
+        public static void CaptureScreenShot(string foldPath , int exceptionIndex)
+        {
+            CheckFoldPath(foldPath);
+            var fileInfo = $"{exceptionIndex}-" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+            var filename = Path.Combine(foldPath, fileInfo);
+            ScreenCapture.CaptureScreenshot(filename);
+        }
 
-    private static void CheckFoldPath(string foldPath)
-    {
-        if (Directory.Exists(foldPath))
-            return;
-        Directory.CreateDirectory(foldPath);
+        private static void CheckFoldPath(string foldPath)
+        {
+            if (Directory.Exists(foldPath))
+                return;
+            Directory.CreateDirectory(foldPath);
+        }
     }
 }
--- Assets/Scripts/LogMonoBehaviour.cs
+++ Assets/Scripts/LogMonoBehaviour.cs
@@ -3,90 +3,93 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class LogMonoBehaviour : MonoBehaviour
+namespace UnityOutputLogSystem
 {
-    private string fileName = "test.txt";
-    private Test   _test;
-    private string _foldePath;
-    private int    _exceptionIndex;
-
-    [SerializeField] private List<ExceptionData> exceptionDatas; 
-
-    private void Start()
+    public class LogMonoBehaviour : MonoBehaviour
     {
-        exceptionDatas                 =  new List<ExceptionData>();
-        _foldePath                     =  Path.Combine(Application.dataPath, "Log");
-        Application.logMessageReceived -= ApplicationOnlogMessageReceived;
-        Application.logMessageReceived += ApplicationOnlogMessageReceived;
-        _test                          =  new Test();
-        _test.Init();
-        SetExceptionIndex();
-        TryToError();
+        private string fileName = "test.txt";
+        private Test   _test;
+        private string _foldePath;
+        private int    _exceptionIndex;
+
+        [SerializeField] private List<ExceptionData> exceptionDatas; 
+
+        private void Start()
+        {
+            exceptionDatas                 =  new List<ExceptionData>();
+            _foldePath                     =  Path.Combine(Application.dataPath, "Log");
+            Application.logMessageReceived -= ApplicationOnlogMessageReceived;
+            Application.logMessageReceived += ApplicationOnlogMessageReceived;
+            _test                          =  new Test();
+            _test.Init();
+            SetExceptionIndex();
+            TryToError();
+        }
+
+        private void SetExceptionIndex()
+        {
+            // _exceptionIndex = 
+            var text       = FileUtility.ReadFile(_foldePath , fileName);
+            var exceptions = text.Split(new []{"Exception Index"} , StringSplitOptions.RemoveEmptyEntries);
+            _exceptionIndex = exceptions.Length;
+        }
+
+
+        private void ApplicationOnlogMessageReceived(string condition, string stacktrace, LogType type)
+        {
+            if (type != LogType.Error && type != LogType.Exception)
+                return;
+            var firstLine = stacktrace.Split('\n')[0];
+            var words = firstLine.Split(new[]
+                                        {
+                                            "Stacktrace : " ," " ,"(at","Assets/Scripts/",":" , "()" , ")"
+                                        } , StringSplitOptions.RemoveEmptyEntries);
+            var lineNumber         = int.Parse(words[2]);
+            var methodAndClassName = words[0].Split('.');
+            var methodName         = methodAndClassName[0];
+            var className          = methodAndClassName[1];
+            if(CheckContains(methodName , className , lineNumber ))  return; // 已存在此行不輸出
+            _exceptionIndex++;
+            string text = $"Exception Index : {_exceptionIndex}\n" + 
+                          $"Data and time : {DateTime.Now}\n"      +
+                          $"Condition : {condition}\n"             +
+                          $"Type : {type}\n"                       +
+                          $"Stacktrace : {stacktrace}";
+            FileUtility.CaptureScreenShot(Path.Combine(_foldePath, "ScreenShot Folder") , _exceptionIndex);
+            FileUtility.WriteFile(_foldePath, text, fileName);
+        }
+
+        private bool CheckContains(string methodName , string ClassName , int lineNumber)
+        {
+            var exceptionData = exceptionDatas.Find(data => data.ClassName           == ClassName && data.MethodName == methodName &&
+                                                            data.ExceptionLineNumber == lineNumber);
+            return exceptionData != null;
+        }
+
+        public void TryToError()
+        {
+            _test.TT();
+        }
+    }
+    public class Test
+    {
+        private int[] _ints;
+
+        public void TT()
+        {
+            ++_ints[1];
+        }
+
+        public void Init()
+        {
+            _ints = new int[1]{ 1 };
+        }
     }
 
-    private void SetExceptionIndex()
+    public class ExceptionData
     {
-        // _exceptionIndex = 
-        var text       = FileUtility.ReadFile(_foldePath , fileName);
-        var exceptions = text.Split(new []{"Exception Index"} , StringSplitOptions.RemoveEmptyEntries);
-        _exceptionIndex = exceptions.Length;
+        public string ClassName;
+        public string MethodName;
+        public int    ExceptionLineNumber;
     }
-
-
-    private void ApplicationOnlogMessageReceived(string condition, string stacktrace, LogType type)
-    {
-        if (type != LogType.Error && type != LogType.Exception)
-            return;
-        var firstLine = stacktrace.Split('\n')[0];
-        var words = firstLine.Split(new[]
-                                    {
-                                        "Stacktrace : " ," " ,"(at","Assets/Scripts/",":" , "()" , ")"
-                                    } , StringSplitOptions.RemoveEmptyEntries);
-        var lineNumber         = int.Parse(words[2]);
-        var methodAndClassName = words[0].Split('.');
-        var methodName         = methodAndClassName[0];
-        var className          = methodAndClassName[1];
-        if(CheckContains(methodName , className , lineNumber ))  return; // 已存在此行不輸出
-        _exceptionIndex++;
-        string text = $"Exception Index : {_exceptionIndex}\n" + 
-                      $"Data and time : {DateTime.Now}\n"      +
-                      $"Condition : {condition}\n"             +
-                      $"Type : {type}\n"                       +
-                      $"Stacktrace : {stacktrace}";
-        FileUtility.CaptureScreenShot(Path.Combine(_foldePath, "ScreenShot Folder") , _exceptionIndex);
-        FileUtility.WriteFile(_foldePath, text, fileName);
-    }
-
-    private bool CheckContains(string methodName , string ClassName , int lineNumber)
-    {
-        var exceptionData = exceptionDatas.Find(data => data.ClassName           == ClassName && data.MethodName == methodName &&
-                                                        data.ExceptionLineNumber == lineNumber);
-        return exceptionData != null;
-    }
-
-    public void TryToError()
-    {
-        _test.TT();
-    }
-}
-public class Test
-{
-    private int[] _ints;
-
-    public void TT()
-    {
-        ++_ints[1];
-    }
-
-    public void Init()
-    {
-        _ints = new int[1]{ 1 };
-    }
-}
-
-public class ExceptionData
-{
-    public string ClassName;
-    public string MethodName;
-    public int    ExceptionLineNumber;
 }
